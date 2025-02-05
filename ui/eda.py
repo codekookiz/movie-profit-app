063d9a96439df3f4f4d609d4a57446edab2debec
codekookiz <codekookiz@gmail.com>
2025-02-06 01:11:59 +0900

    a

--- ui/eda.py
+++ ui/eda.py
@@ -9,7 +9,7 @@ from matplotlib import rc
 
 @st.cache_data
 def fontRegistered():
-    font_dirs = [os.getcwd() + '/custom_fonts']
+    font_dirs = [os.getcwd() + '../custom_fonts']
     font_files = fm.findSystemFonts(fontpaths=font_dirs)
     for font_file in font_files:
         fm.fontManager.addfont(font_file)
